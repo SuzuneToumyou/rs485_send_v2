3c4c1fc249d093d40720c35fa5d40d9063325422
ぬこむーちょ <30884264+SuzuneToumyou@users.noreply.github.com>
2023-08-24 11:17:15 +0900

    Create send_data.py

---
+++ send_data.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+
+import serial
+import time
+import pigpio
+
+THERMO_HIGT = 85
+THERMO_LOW = 60
+
+pi2 = pigpio.pi()
+
+ser_v = serial.Serial('/dev/ttyUSB0', 19200, timeout=None)
+
+stop_times = 0.77
+
+def senser_get(ser,pi):
+
+    d_ed = "big"
+
+    s_code = b'\x02'
+    s_ID = b'\x00'
+    addr = 0x0a
+
+    try:
+        h = pi.i2c_open(1,addr)
+        pi.i2c_write_device(h, [0x4d])
+        time.sleep(stop_times)
+        count, result = pi.i2c_read_device(h,2051)
+        time.sleep(stop_times)
+        pi.i2c_close(h)
+    except:
+        count = -80
+
+#    print(count)
+#    print(result)
+
+    if count <= 0: #device_error
+        return (0)
+    else:
+        readbuff = bytes(result)
+
+        s_bit = b"\x02"
+
+        ser.write(s_bit)
+
+        ser.write(s_code)
+
+        ser.write(s_ID)
+
+        tmp_termo = 0
+
+        for i in range(1025):
+            ttmp = readbuff[i*2]
+            msg = ttmp.to_bytes(1,d_ed)
+            ser.write(msg)
+
+            tttmp = readbuff[i*2+1]
+            msg = tttmp.to_bytes(1,d_ed)
+            ser.write(msg)
+            
+            if(tmp_termo <= (256 * tttmp + ttmp)/10):
+                tmp_termo = (256 * tttmp + ttmp)/10
+        
+        tPEC = readbuff[2050]
+        msg = tPEC.to_bytes(1,d_ed)
+        ser.write(msg)
+        
+        #add 2023/08/23
+        if (tmp_termo >= THERMO_HIGT):
+            msg = b"\x02"
+        else if (tmp_termo >= THERMO_LOW):
+            msg = b"\x01"
+        else:
+            msg = b"\x00"
+        ser.write(msg)
+
+        
+
+        msg = b"\x03"
+        ser.write(msg)
+
+        return(1)
+
+if __name__ == "__main__":
+
+    return_data = senser_get(ser_v,pi2)
+    if return_data == 0:
+        num = 0
+        while return_data == 0 or num <= 3:
+            return_data = senser_get(ser_v,pi2)
+            num = num + 1
